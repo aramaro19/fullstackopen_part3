2eaedbbbc7e7dae0f3929305a5605f0b80479396
Alejandro Robert <75962547+aramaro19@users.noreply.github.com>
2024-06-15 11:01:41 -0400

    part 3.5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
         "id": 1,
@@ -54,6 +56,24 @@ app.delete("/api/persons/:id", (request, response) => {
     persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
+})
+
+app.post("/api/persons", (request, response) => {
+    const body = request.body
+    if(!body.name) {
+        return response.status(400).json({
+            error: "content missing"
+        })
+    } else {
+        const person = {
+            ...body,
+            id: Math.floor( Math.random() * 10000)
+        }
+
+        persons = persons.concat(person)
+        response.json(person)
+    }
+
 })
 
 const PORT = 3001
